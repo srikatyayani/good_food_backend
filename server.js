df06391dcdb50ddc7319c6386b3bcb03abad656a
srikatyayani <srikatyayani2003@gmail.com>
2025-06-27 18:38:40 +0530

    change in server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const path = require("path");
 const app = express();
 
 dotenv.config(); //using this we can access the .env file
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // or "*", but not recommended for prod
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URL)
