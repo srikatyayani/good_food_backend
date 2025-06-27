21a9fe988911e67c75fa8d7c83587c3b857bc721
srikatyayani <srikatyayani2003@gmail.com>
2025-06-27 21:20:16 +0530

    change in firmController.js

--- routes/firmRouter.js
+++ routes/firmRouter.js
@@ -4,7 +4,7 @@ const verifyToken = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
-router.post("/add-firm", verifyToken, firmController.addFirm);
+router.post("/add-firm", verifyToken, ...firmController.addFirm);
 
 router.get("/uploads/:imageName", (req, res) => {
   const imageName = req.params.imageName;
