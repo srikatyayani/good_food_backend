181b09170d3cd90a98ffa9f0f74dfd9642e01101
srikatyayani <srikatyayani2003@gmail.com>
2025-06-28 10:24:01 +0530

    change in firmController.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotEnv = require("dotenv");
 const mongoose = require("mongoose");
-const vendorRoutes = require("./routes/vendorRoutes");
 const bodyParser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
 const productRoutes = require("./routes/productRoutes");
